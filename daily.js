4c5fc10332cba132570949e1dfff641184a2172a
Vilmar-Dev <ammasivilmar2@gmail.com>
2026-05-19 21:33:35 +0800

    update

--- daily.js
+++ daily.js
@@ -929,6 +929,37 @@ let i=1;
             
 
 
+    
+
+    //Day 13 Task 1, Task 2
+    //Make a program that will create a function
+    //Make a program that will call the function
+
+
+     function greet(){
+        console.log('HEllo');
+    }
+
+    greet();
+
+
+    //Day 13 Task 3
+    //Make a program that will create subtract function
+
+    function subtract() {
+    
+    let num1=10;
+    let num2=20;
+    let sub=num1-num2;
+        console.log(sub);
+
+
+   }    
+
+   subtract();
+
+
+
    
 
 
